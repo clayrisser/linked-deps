bbb953c9de88c3b34c419fbc406e600f1936ad2d
Jam Risser <jam@codejam.ninja>
2019-10-01 20:13:21 -0500

    Added types

---
+++ src/index.d.ts
@@ -0,0 +1,7 @@
+declare module 'linked-deps' {
+  export function isLinked(dependency: string): boolean;
+
+  export function getLinked(pkgPath?: string): string[];
+
+  export function getLinkedPaths(pkgPath: string): string[];
+}
